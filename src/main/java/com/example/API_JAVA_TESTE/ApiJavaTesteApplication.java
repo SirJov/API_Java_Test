c15b1bc9e451e1eae9a1c2dd6430042d2f9ebb02
Heric_ <herico_campos@hotmail.com>
2023-09-07 17:32:32 -0300

    primeiro commit

---
+++ src/main/java/com/example/API_JAVA_TESTE/ApiJavaTesteApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_JAVA_TESTE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/API_JAVA_TESTE/ApiJavaTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_JAVA_TESTE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
